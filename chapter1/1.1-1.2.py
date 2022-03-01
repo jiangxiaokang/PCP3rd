20453eaf9bbf1cce5cd1c042250f8c040bd8c13c
jiangxiaokang <385807587@qq.com>
2022-03-01 21:21:54 +0800

    1-3

--- chapter1/1.1-1.2.py
+++ chapter1/1.1-1.2.py
@@ -2,14 +2,28 @@ import re
 
 """ 1-1 bat/bit/but/hat/hit/hut"""
 str='bit'
-m = re.match(r'[b|h][a|i|u]t',str)
+m = re.match(r'[bh][aiu]t',str)
 print(m.group())
 """ 1-2 """
 str="Jack Ma"
 m = re.match(r'[\w]+ [\w]+',str)
 print(m.group())
 
-str="Jack"
-m = re.match(r'[\w]+ [\w]+',str)
+str="Jack 3321"
+m = re.match(r'[A-Za-z]+ [A-Za-z]+',str)
 if m is not None:print(m.group())
 else: print('not match')
+
+""" 1-3 """
+str='Isaac Newton'
+str1="Jack M"
+str2="Zhang,Fei"
+str3='Li,B'
+def test_1_3(str):
+    m = re.findall('\w+',str,re.I)
+    print(m)
+
+test_1_3(str)
+test_1_3(str1)
+test_1_3(str2)
+test_1_3(str3)
