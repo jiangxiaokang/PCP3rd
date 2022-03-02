ed6e40b7c9117ae4417f81f92ad102f62108b73b
jiangxiaokang <385807587@qq.com>
2022-03-03 00:08:38 +0800

    2.6 for python2.7 and py3

--- chapter2/2.6_client.py
+++ chapter2/2.6_client.py
@@ -2,6 +2,7 @@ import socket
 HOST='localhost'
 PORT=50007
 BUFSIZ = 1024
+"""
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
     while True:
@@ -15,3 +16,17 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
         if not data:
             break
         print(data.decode('utf-8'))
+"""
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+while True:
+    #s.sendall(b'hello, world')
+    data = raw_input('>')
+    if not data:
+        break
+    s.sendall(data.encode('utf-8'))
+
+    data=s.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
--- chapter2/2.6_server.py
+++ chapter2/2.6_server.py
@@ -5,23 +5,44 @@ import time
 BUFSIZ = 1024
 HOST='localhost'
 PORT=50007
-
+"""
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     s.bind((HOST,PORT))
     s.listen(1)
     while True:
         conn,add = s.accept()
         print("connected by ",add)
+        while True:
+            data = conn.recv(BUFSIZ)
+            if not data:break
+            msg = data.decode('utf-8')
+            print("receive msg",msg)
+            if msg == 'os':
+                conn.send(os.name.encode('utf-8'))
+            elif msg == 'ls':
+                conn.send(str(os.listdir()).encode('utf-8'))
+            else:
+                conn.send(time.ctime().encode('utf-8'))
+        conn.close()
+"""
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((HOST,PORT))
+s.listen(1)
+while True:
+    conn,add = s.accept()
+    print("connected by ",add)
+    while True:
         data = conn.recv(BUFSIZ)
         if not data:break
-
         msg = data.decode('utf-8')
+        print("receive msg",msg)
         if msg == 'os':
             conn.send(os.name.encode('utf-8'))
         elif msg == 'ls':
             conn.send(str(os.listdir()).encode('utf-8'))
         else:
             conn.send(time.ctime().encode('utf-8'))
-        conn.sendall(data)
+        ##conn.sendall(data)
+    conn.close()
 
     
